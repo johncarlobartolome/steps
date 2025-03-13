28f056c83f66b06c2ecf94df34e257338cd15186
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-03-13 13:22:30 +0800

    Implement step navigation with messages and buttons in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const step = 2;
+
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
